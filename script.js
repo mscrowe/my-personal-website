7b6189e675cac5fb79d43f4b96b5fa2c2781d905
Marcello <marcello.crowe@yahoo.com>
2023-10-01 21:01:24 -0600

    Added links and did Javascript

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ const socialMedia=["IMDB, Podcast, Coverfly, Instagram"];
 
 console.log(socialMedia);
 
-var elements = document.getElementsByTagName('a');
+const socialMedia = {
+    IMDB: URL"https://www.imdb.com/name/nm9675069/",
+    Podcast: URL "https://podcasters.spotify.com/pod/show/shufflebox",
+    Coverfly: URL "https://writers.coverfly.com/profile/writer-e906b6d39-57624",
+    Instagram: URL "https://www.instagram.com/crowe303/",
+}
+
+socialMedia.IMDB;
+socialMedia.Podcast;
+socialMedia.Coverfly;
+socialMedia.Instagram;
+
